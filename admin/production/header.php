48f2d98afb3bde13fc5aa1a5bd66f3e9d6d42231
Akif Güldemir <akifguldemir@gmail.com>
2022-05-06 10:29:35 +0300

    veritabanı bağlantısı yapıldı

--- admin/production/header.php
+++ admin/production/header.php
@@ -1,3 +1,9 @@
+<?php 
+
+include '../netting/baglan.php';
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -7,7 +13,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>DataTables | Gentelella</title>
+    <title>BonteSoft Banner</title>
 
     <!-- Bootstrap -->
     <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -34,21 +40,13 @@
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>Gentelella Alela!</span></a>
+              <a href="index.php" class="site_title"><i class="fa fa-paw"></i> <span>Bonte Soft Banner</span></a>
             </div>
 
             <div class="clearfix"></div>
 
             <!-- menu profile quick info -->
-            <div class="profile clearfix">
-              <div class="profile_pic">
-                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
-              </div>
-              <div class="profile_info">
-                <span>Welcome,</span>
-                <h2>John Doe</h2>
-              </div>
-            </div>
+           
             <!-- /menu profile quick info -->
 
             <br />
@@ -58,7 +56,7 @@
               <div class="menu_section">
                 <h3>General</h3>
                 <ul class="nav side-menu">
-                  <li><a><i class="fa fa-home"></i> Banner Sayfası <span class="fa fa-chevron-down"></span></a>
+                  <li><a href="index.php"><i class="fa fa-home"></i> Banner Sayfası <span class="fa fa-chevron-down"></span></a>
                   <li><a href="banner-ekle.php"><i class="fa fa-home"></i> Banner Ekle <span class="fa fa-chevron-down"></span></a>
 
                   
